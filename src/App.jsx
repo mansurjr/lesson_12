ecdce1068aa4d8526503695ea727ff6e55e6105c
mansurjr <ztm007uzb@gmail.com>
2025-08-26 13:10:06 +0500

    Updated'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Footer from "./components/footer/footer";
-import Main from "./components/main/Main";
 import UserApp from "./components/User";
 import Container from "./components/container/Container";
 import "@fontsource-variable/roboto";
